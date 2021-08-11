b8b4d4077779438f8a53ee6a40093ca525ad5c31
JebastinManiraj <88772888+JebastinManiraj@users.noreply.github.com>
2021-08-11 15:26:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+function setup() {
+  createCanvas(400,400);
+  var box =createSprite(200,200,50,50);
+console.log("welcome");
+
+
+
+
+}
+
+function draw() 
+{
+  background("yellow");
+drawSprites();
+}
+
+
+
+
